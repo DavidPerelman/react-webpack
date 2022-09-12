a45ebb1b296af30d9229d0bed3884fa6f11158cc
David Perelman <dperelman3@gmail.com>
2022-09-12 19:49:39 +0300

    add link to the guide

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-const App = () => <h1>Hello from React!</h1>;
+const App = () => (
+  <div>
+    <h1>Hello from React!</h1>
+    <a
+      href='https://jsramblings.com/creating-a-react-app-with-webpack/'
+      target='_blank'
+    >
+      Click Me!
+    </a>
+  </div>
+);
 
 export default App;
